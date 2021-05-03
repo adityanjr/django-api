63ea5f81c1018d9da0b60412443d2953b177582c
adityanjr <samant04aditya@gmail.com>
2021-05-03 17:05:41 +0530

    basic_auth

--- apiREST/views.py
+++ apiREST/views.py
@@ -1,13 +1,18 @@
 from .models import Article
 from .serializer import ArticleSerializer
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
